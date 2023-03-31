262525cca565e50c9d653f7a88fb35e5afcfde9b
Valentino Traverso <vt20032003@live.com.ar>
2023-03-31 10:51:54 +0200

    add: toast

--- src/views/components/AddWish/AddWish.jsx
+++ src/views/components/AddWish/AddWish.jsx
@@ -4,6 +4,8 @@ import useWishContext from "../../../context/WishContext";
 import { localStorage, addWish } from "../../../utils/utils";
 import './AddWish.css';
 import postTask from "../../../api/postTask";
+import { ToastContainer, toast  } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.min.css'
 
 export default function AddWish() {
     const [wishTitle, setWishTitle] = useState({ title: '', status: 0 });
@@ -12,18 +14,51 @@ export default function AddWish() {
         e.preventDefault();
 
         if (wishTitle.title === null || wishTitle.title === '') {
-            alert('Must fill the input with text!');
+            toast.error('Must fill the input with text!😡', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
             return;
         }
 
 
         const { status } = await postTask(wishTitle);
 
-        (status === 'Upload') && setWishTitle({...wishTitle, title: ''});
+        (status === 'Upload') ? 
+        setWishTitle({ ...wishTitle, title: '' }) 
+        :
+        toast.error('There was an error!😭', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        });
     }
 
     return (
         <div className="add-wish__div">
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+            />
             <form onSubmit={handleSubmit}>
                 <input type="text" name="wishTitle" value={wishTitle.title} placeholder='🖊️ Walk the dog' onChange={(e) => { setWishTitle({ ...wishTitle, title: e.target.value }) }} />
             </form>
